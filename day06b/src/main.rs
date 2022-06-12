1fd6f9e864b7dde5c4cba0dbf702da39293d3223
Henry Wang <henrywang97@gmail.com>
2022-06-12 20:17:45 +0100

    add day06b

---
+++ day06b/src/main.rs
@@ -0,0 +1,32 @@
+use std::iter::repeat;
+
+fn solution(input: &str, days: usize) -> usize {
+    let mut fish_counts = vec![0; 9];
+    for f in input.split(',').map(|n| n.parse::<usize>().unwrap()) {
+        fish_counts[f] += 1;
+    }
+
+    let final_count = repeat(1)
+        .scan(fish_counts, |fish_counts, _| {
+            let zeroes = fish_counts[0];
+            fish_counts.rotate_left(1);
+            fish_counts[6] += zeroes;
+            Some(fish_counts.clone())
+        })
+        .take(days)
+        .last()
+        .unwrap()
+        .iter()
+        .sum();
+
+    final_count
+}
+
+fn main() {
+    let test_input = "3,4,3,1,2";
+    assert_eq!(solution(test_input, 80), 5934);
+    assert_eq!(solution(test_input, 256), 26984457539);
+    let input = include_str!("../inputs.txt").trim();
+    println!("{}", solution(input, 80));
+    println!("{}", solution(input, 256));
+}
